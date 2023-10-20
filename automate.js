60ad41aed3408d8e551d25e1f02ca603198350f6
Gustavo Antonio Martini <gutamen@live.com>
2023-10-20 18:25:44 -0300

    auto

--- automate.js
+++ automate.js
@@ -1,1 +1,39 @@
-console.log('jegue');
+const fs = require("fs");
+
+//console.log('teste');
+
+let file = fs.readFileSync("teste", "utf-8");
+
+
+
+let state = 0;
+
+let readString = "";
+
+let iterator = 0;
+
+while(iterator < file.length){
+   
+    //console.log(file.at(iterator));
+
+    let char = file.at(iterator);
+
+    readString += char;
+
+    if(state == 0){
+       if(char in "A-Z"){
+
+        } 
+
+        
+    }
+
+    iterator++;
+        
+
+}
+
+//console.log(readString);
+
+//if(readString === file)
+    //console.log('at');
