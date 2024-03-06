46afdbae5ff8b0c84c58195a10523811a26eb8ce
Gustavo Antonio Martini <gutamen@live.com>
2024-03-06 15:57:46 -0300

    começo do analisador sintático implementado

--- sintatic.js
+++ sintatic.js
@@ -1,8 +1,38 @@
 
 let sintaticTopDown = function(tokens){
-    for (let index = 0; index < tokens.length; index++) {
-        console.log(tokens[index]);
-    }
+    let stack = [];
+    stack.push("$", "<programa>");
+    tokens.push(["","$", 0, 0]);
+    
+    console.log(stack);
+    console.log(tokens);
+    while(true){
+        let stackTop = stack[stack.length - 1];
+        let tokenFirst = tokens[0][1];
+        
+        console.log(tokenFirst);
+
+        if(stackTop.includes("<") && stackTop.includes(">")){ // é um não terminal
+
+            switch(stackTop){
+                case "<programa>":
+                    switch(tokenFirst){
+                                                            
+
+                        default:
+                            console.log("token Inválido");
+                    }
+                break;
+
+                default:
+                    console.log("Erro: não terminal não existe, ou não foi colocado")
+            }
+        }
+        else{ // é um terminal
+        }
+
+        break;
+    }        
 }
 
 module.exports = sintaticTopDown;
